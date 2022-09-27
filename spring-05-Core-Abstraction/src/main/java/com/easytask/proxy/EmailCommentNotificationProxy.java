b455fac1c0f303cd7cac8c0e97057ab53d85c04a
Shukur A <sukuri33@gmail.com>
2022-09-26 23:05:28 -0500

    Created another impl class for CommentNotification interface to demonstrate @Qualifier annotation

--- spring-05-Core-Abstraction/src/main/java/com/easytask/proxy/EmailCommentNotificationProxy.java
+++ spring-05-Core-Abstraction/src/main/java/com/easytask/proxy/EmailCommentNotificationProxy.java
@@ -1,6 +1,7 @@
 package com.easytask.proxy;
 
 import com.easytask.model.Comment;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
--- spring-05-Core-Abstraction/src/main/java/com/easytask/service/CommentService.java
+++ spring-05-Core-Abstraction/src/main/java/com/easytask/service/CommentService.java
@@ -4,15 +4,25 @@ import com.easytask.model.Comment;
 import com.easytask.proxy.CommentNotificationProxy;
 import com.easytask.repo.CommentRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 public class CommentService {
     //#1 Scenario: by good practice 'final' keyword is recommended so that you wouldn't forget to create constructor
     //#2 Scenario: sometimes you declare field and forgot to add them in the constructor argument, if you have a habit of using final keyword it would help you avoid such mistake
     private final CommentRepository commentRepository;
     private final CommentNotificationProxy commentNotificationProxy;
+
+                                                             //in the event that your 2nd implementation class of CommentNotificationProxy interface
+                                                             //in this case it is "PushNotificationProxy", is needed for a different Service class
+                                                             //And you used @Primary annotation for "EmailCommentNotificationProxy" class to tell Spring avoid using the 2nd impl class
+                                                             //then the other Service class would also use EmailProxy class.
+                                                             //To address such concern we have @Qualifier annotation in Spring
+    public CommentService(CommentRepository commentRepository,@Qualifier("emailCommentNotificationProxy") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
 
     public void publishComment(Comment comment){
 
